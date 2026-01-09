dfb2f86047c7b9c9b6042410bb3aeca99754094b
XD0N <88358673+XD0N@users.noreply.github.com>
2026-01-10 05:43:55 +0800

    xdon jpg

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import avatarImg from "@/public/images/avatar.png";
+import avatarImg from "@/public/images/avatar.jpg";
 
 export default function Avatar({ page }) {
   return (
